c48f8409482e9bc6cf753314901854e1dab3813d
couperin123 <46934628+couperin123@users.noreply.github.com>
2020-09-14 22:31:07 -0700

    remove datetime gap

--- app/app.py
+++ app/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, flash
 # from app import app
 from forms import TickerForm
 from config import Config
+from math import pi
 
 import os
 import requests
@@ -45,12 +46,17 @@ def index():
             month = df.loc[df.index.max():(df.index.max() - pd.DateOffset(months=1))]
 
             # make_plot
-            p = figure(x_axis_type="datetime", title=r.json()['Meta Data']['2. Symbol'].upper(), plot_height=350, plot_width=800)
+            p = figure(title=r.json()['Meta Data']['2. Symbol'].upper(), plot_height=350, plot_width=800)
+            p.xaxis.ticker = list(range(len(month.index)))
+            p.xaxis.major_label_overrides = {
+                i: date.strftime('%b %d') for i, date in enumerate(month.index[::-1])
+            }
+            p.xaxis.major_label_orientation = pi/4
             p.xgrid.grid_line_color=None
             p.ygrid.grid_line_alpha=0.5
             p.xaxis.axis_label = 'Date'
             p.yaxis.axis_label = 'Closing Price'
-            p.line(month.index, month['Closing Price'], line_width=4)
+            p.line(range(len(month.index)), month['Closing Price'], line_width=4)
             script, div = components(p)
 
             return render_template('dashboard.html', plots=[(script, div)])
